9db0b0d03c6d4e58ecf41e4f1bf97b77c5e041e8
1605200517 <1605200517@users.noreply.github.com>
2017-10-27 17:48:33 +0200

    Create silent-upgrade.sh

---
+++ ubuntu/silent-upgrade.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical \ 
+apt-get -q -y -o "Dpkg::Options::=--force-confdef" -o \
+"Dpkg::Options::=--force-confold" upgrade && apt-get -y autoremove
