ef2a52e772fc56c426a9efdce35784d8539885a4
1605200517 <1605200517@users.noreply.github.com>
2017-10-28 11:53:47 +0200

    Create install-debian-9-stretch.sh

---
+++ docker/install-debian-9-stretch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt-get -y install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+   $(lsb_release -cs) \
+   stable"
+   
+  apt-get -y update && apt-get -y install docker-ce 
+  
+  
