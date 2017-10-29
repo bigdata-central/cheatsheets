29bc876a7d642896df79fb5663d4451c05cf1af2
1605200517 <1605200517@users.noreply.github.com>
2017-10-29 17:49:18 +0100

    Update install-debian-9-stretch.sh

--- docker/install-debian-9-stretch.sh
+++ docker/install-debian-9-stretch.sh
@@ -9,7 +9,7 @@ apt-get -y install \
      gnupg2 \
      software-properties-common
 
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
 
 apt-key fingerprint 0EBFCD88
 
@@ -18,7 +18,7 @@ add-apt-repository \
    $(lsb_release -cs) \
    stable"
    
-  apt-get -y update && apt-get -y install docker-ce --allow-unauthenticated
+  apt-get -y update && apt-get -y install docker-ce 
 
 
   
